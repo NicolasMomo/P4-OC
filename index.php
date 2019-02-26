03a68a21f339508410628aa952c605802e5426ae
NicolasM <nicolas.moreno@outlook.fr>
2019-02-26 16:48:28 +0100

    Create index.php
    Création d'un fichier index.php contenant les $_GET['action'] permettant des voir les posts

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+
+require('controller/frontend.php');
+
+try { // On essaie de faire des choses
+    if (isset($_GET['action'])) {
+        if ($_GET['action'] == 'listPosts') {
+            listPosts();
+        }
+        elseif ($_GET['action'] == 'post') {
+            if (isset($_GET['id']) && $_GET['id'] > 0) {
+                post($_GET['id']);
+            }
+            else {
+                // Erreur ! On arrête tout, on envoie une exception, donc au saute directement au catch
+                throw new Exception('Aucun identifiant de billet envoyé');
+            }
+        }
+    }
+    else {
+        listPosts();
+    }
+}
+catch(Exception $e) { // S'il y a eu une erreur, alors...
+    echo 'Erreur : ' . $e->getMessage();
+}
