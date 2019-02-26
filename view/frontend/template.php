03e992b16c4e5fb029428d1693fb967a96b49192
NicolasM <nicolas.moreno@outlook.fr>
2019-02-26 17:33:54 +0100

    Update template.php
    Ajout d'une session utilisateur, permettant au membre de poster des commentaires, se déconnecter lorsqu'il a fini sa visite. Ajout d'un lien "Administration" qui s'affichera seulement lorsque l'admnistrateur sera connecté

--- view/frontend/template.php
+++ view/frontend/template.php
@@ -10,9 +10,41 @@
 </head>
 
 <body>
-    <div id="welcomeLinks">
-        <a href="index.php?action=connectForm">Se connecter</a>
-        <a href="index.php?action=inscriptionForm">S'inscrire</a>
+    <div id="welcomeConnect">
+        <div id="welcome">
+            <?php
+                if (isset($_SESSION['id']) AND isset($_SESSION['pseudo']))
+                {
+                    echo 'Bonjour ' . $_SESSION['pseudo'];
+            ?>
+            <br /><a href="index.php?action=disconnectMember">Se déconnecter</a>
+            <?php
+                }
+            ?>
+        </div>
+        <?php
+            if (!isset($_SESSION['id']) AND !isset($_SESSION['pseudo']))
+                {
+        ?>
+        <div id="welcomeLinks">
+            <a href="index.php?action=connectForm">Se connecter</a>
+            <a href="index.php?action=inscriptionForm">S'inscrire</a>
+            <?php
+                }
+        ?>
+        </div>
+
+        <?= $content ?>
+        <?php
+        if (isset($_SESSION['admin']) AND ($_SESSION['admin'] == 1))
+        {
+        ?>
+        <div id="connectAdmin">
+            <a href="index.php?action=seeReported">Administration</a>
+        </div>
+        <?php
+        }
+    ?>
     </div>
 </body>
 
