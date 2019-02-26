f63a09c4e2474b24c161010aa8db966fef0ae87b
NicolasM <nicolas.moreno@outlook.fr>
2019-02-26 18:04:02 +0100

    Create modifyPosts.php
    Création d'une page modifyPosts.php, permettant de modifier un post déjà publié

---
+++ view/frontend/modifyPosts.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <script src="https://cloud.tinymce.com/5/tinymce.min.js"></script>
+    <script>tinymce.init({ selector:'textarea' });</script>
+</head>
+
+<body>
+    <div id="WrittingChap">
+
+        <h2>Modifier chapitre</h2>
+        <?php
+					while($chapitre = $post->fetch()){
+				?>
+        <form id="getNewChapter" action="index.php?action=modifyPost" method="post">
+
+            <label>Titre:<input type="text" name="title" id="title" value="<?= htmlspecialchars($chapitre['title']) ?>" required /></label>
+
+            <textarea class="tinymce" name="tinymce_Chap"><?= nl2br(htmlspecialchars($chapitre['content'])) ?></textarea>
+
+            <input type="submit" id="edit" value="Modifier" />
+        </form>
+        <?php
+					}
+					$post->closeCursor();
+				?>
+    </div>
+</body>
+
+</html>
