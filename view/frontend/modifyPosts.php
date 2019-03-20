dfc8c5e3fd203f6a21fbab6d34e02c7f3d9bd007
NicolasM <nicolas.moreno@outlook.fr>
2019-03-20 10:44:50 +0100

    Update modifyPosts.php
    Ajout de plusieurs <div> pour le CSS

--- view/frontend/modifyPosts.php
+++ view/frontend/modifyPosts.php
@@ -2,22 +2,32 @@
 <html>
 
 <head>
+    <link href="public/css/style.css" rel="stylesheet" />
     <script src="https://cloud.tinymce.com/5/tinymce.min.js"></script>
-    <script>tinymce.init({ selector:'textarea' });</script>
+    <script>tinymce.init({ selector:'textarea', height: '600px' });</script>
 </head>
 
 <body>
+
+    <h1>Modifier chapitre</h1>
+
+    <a href="index.php?action=seeReported">&larr; Retour à l'administration</a>
+
+    <div id="background3">
+        <div id="separateur"></div>
+        <div id="separateur2"></div>
+    </div>
+
     <div id="WrittingChap">
 
-        <h2>Modifier chapitre</h2>
         <?php
 					while($chapitre = $post->fetch()){
 				?>
         <form id="getNewChapter" action="index.php?action=modifyPost&amp;id=<?php echo $chapitre['id']; ?>" method="post">
 
-            <label>Titre:<input type="text" name="title" id="title" value="<?= htmlspecialchars($chapitre['title']) ?>" required /></label>
+            <label id="modifyPostLabel">Titre:<input type="text" name="title" id="title" value="<?= htmlspecialchars($chapitre['title']) ?>" required /></label>
 
-            <textarea class="tinymce" name="tinymce_Chap"><?= nl2br(htmlspecialchars($chapitre['content'])) ?></textarea>
+            <textarea class="tinymce" name="tinymce_Chap"><?= $chapitre['content'] ?></textarea>
 
             <input type="submit" id="edit" value="Modifier" />
         </form>
