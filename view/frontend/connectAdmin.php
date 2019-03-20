883617e0b173f86fe5cb4fd0338822f62dfe9952
NicolasM <nicolas.moreno@outlook.fr>
2019-03-20 11:03:16 +0100

    Update connectAdmin.php
    Refonte de la page d'administration, ajout de <div> pour le CSS, affichage des commentaires signalés, des posts déjà présent sur le site et la possibilité de les modifier, ainsi qu'un espace permettant l'écriture et la publication de nouveaux posts. Ajout d'un fichier JavaScript permettant en fonction du bouton choisi, d'affichier seulement les commentaires signalés, les posts déjà présent sur le site ou l'espace permettant l'écriture et la publication de nouveaux posts

--- view/frontend/connectAdmin.php
+++ view/frontend/connectAdmin.php
@@ -2,16 +2,44 @@
 
 <?php ob_start(); ?>
 
-<h1>Espace d'administration</h1>
+<h1 class='welcomeTitle'>ESPACE D'ADMINISTRATION</h1>
+
+<a href="index.php">&larr; Retour à l'accueil</a>
+
+<?php
+        if (isset($_SESSION['admin']) AND $_SESSION['admin'] == 1)
+        {
+        ?>
+<div id="adminLinks">
+    <div class="adminLinksBox">
+        <a href="#" id="newChapter">Écrire un nouveau chapitre</a>
+        <img class="newPostsImg" src="public/img/posts.png" />
+    </div>
+    <div class="adminLinksBox">
+        <a href="#" id="showComments">Modération des commentaires</a>
+        <img id="commentsImg" src="public/img/comments.png" />
+    </div>
+    <div class="adminLinksBox">
+        <a href="#" id="showPosts">Gestion des posts</a>
+        <img class="newPostsImg" src="public/img/modifyposts.png" />
+    </div>
+</div>
+
+<div id="background3">
+    <div id="separateur"></div>
+    <div id="separateur2"></div>
+</div>
+
 <div id="adminSpace">
 
     <div id="WrittingChap">
-
-        <h2>Nouveau chapitre</h2>
+        <div class="titles">
+            <h2>Écriture d'un nouveau chapitre</h2>
+        </div>
 
         <form id="getNewChapter" action="index.php?action=addPost" method="post">
 
-            <label>Titre:<input type="text" name="title" id="title" value="" required /></label>
+            <label id='postTitle'>Titre:<input type="text" name="title" id="title" value="" required /></label>
 
             <textarea class="tinymce" name="tinymce_Chap"></textarea>
 
@@ -19,8 +47,9 @@
         </form>
     </div>
 
+
     <div id="adminComments">
-        <div>
+        <div class="titles">
             <h2>Modération des commentaires</h2>
         </div>
         <div>
@@ -28,7 +57,7 @@
     foreach ($reported as $report)
     {
     ?>
-            <div id="commentZone">
+            <div id="adminCommentZone">
                 <p id="authorNameDate"><strong>
                         <?= htmlspecialchars($report['author']) ?></strong> le
                     <?= $report['comment_date_fr'] ?>
@@ -40,13 +69,13 @@
                 <a href="index.php?action=deleteComment&amp;id=<?= $report['id'] ?>">Supprimer</a>
             </div>
             <?php
-    }
-    ?>
+            }
+            ?>
         </div>
     </div>
 
     <div id="adminPosts">
-        <div>
+        <div class="titles">
             <h2>Modération des posts</h2>
         </div>
         <div>
@@ -61,12 +90,13 @@ while ($data = $posts->fetch())
                         <?= $data['creation_date_fr'] ?></em>
                 </h3>
 
+                <div id="newsAdminContent">
+                    <?= nl2br($data['content']) ?>
 
-                <?= nl2br($data['content']) ?>
-                <br />
 
-                <a href="index.php?action=seePost&amp;id=<?= $data['id'] ?>">Modifier</a>
-                <a href="index.php?action=deletePost&amp;id=<?= $data['id'] ?>">Supprimer</a>
+                    <a href="index.php?action=seePost&amp;id=<?= $data['id'] ?>">Modifier</a>
+                    <a href="index.php?action=deletePost&amp;id=<?= $data['id'] ?>">Supprimer</a>
+                </div>
             </div>
             <?php
 }
@@ -75,11 +105,21 @@ $posts->closeCursor();
         </div>
     </div>
 </div>
+<?php
+        }
+else{
+    ?>
+<h1>Zone réservée à l'administrateur</h1>
+<?php
+}
+?>
 
+<script src="public/js/blog.js"></script>
 <script src="https://cloud.tinymce.com/5/tinymce.min.js"></script>
 <script>
     tinymce.init({
-        selector: 'textarea'
+        selector: 'textarea',
+        height: '600px'
     });
 
 </script>
