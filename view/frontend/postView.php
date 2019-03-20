b1e9c230f3ffb3313dab302445bf187b0be0bb40
NicolasM <nicolas.moreno@outlook.fr>
2019-03-20 10:42:55 +0100

    Update postView.php
    Ajout de plusieurs <div> pour le CSS

--- view/frontend/postView.php
+++ view/frontend/postView.php
@@ -46,7 +46,84 @@
     ?>
 
 <?php
+while ($comment = $comments->fetch())<?php $title = htmlspecialchars($post['title']); ?>
+
+<?php ob_start(); ?>
+<h1>Billet simple pour l'Alaska !</h1>
+<p><a href="index.php">&larr; Retour à la liste des billets</a></p>
+
+<div id="background2">
+    <div id="separateur"></div>
+    <div id="separateur2"></div>
+</div>
+
+<div class="news">
+    <h3>
+        <?= $post['title'] ?>
+        <em>le
+            <?= $post['creation_date_fr'] ?></em>
+    </h3>
+    <div class="newsContent">
+        <p>
+            <?= $post['content'] ?>
+        </p>
+    </div>
+</div>
+
+<h2 id="titleComments">Écrire un commentaire</h2>
+<div>
+    <?php
+        if (isset($_SESSION['id']) AND $_SESSION['pseudo'])
+            {
+            ?>
+    <div id="commentBlock">
+        <form id="postComment" action="index.php?action=addComment&amp;id=<?= $post['id'] ?>" method="post">
+            <div>
+                <label for="author">Auteur</label><br />
+                <input type="text" id="author" name="author" value="<?php echo $_SESSION['pseudo'] ?>" />
+            </div>
+            <div>
+                <label for="comment">Commentaire</label><br />
+                <textarea id="comment" name="comment"></textarea>
+            </div>
+            <div>
+                <input type="submit" />
+            </div>
+        </form>
+    </div>
+    <?php
+            }
+            else
+                {
+                ?>
+    <p id="connectForComment">Veuillez vous connecter afin de pouvoir poster un commentaire.</p>
+    <?php
+                }
+                ?>
+    <h2 id="titleComments">Commentaires</h2>
+    <?php
 while ($comment = $comments->fetch())
+    {
+    ?>
+    <div id="commentZone">
+        <p id="authorNameDate"><strong>
+                <?= htmlspecialchars($comment['author']) ?></strong> le
+            <?= $comment['comment_date_fr'] ?>
+        </p>
+        <p id="commentText">
+            <?= nl2br(htmlspecialchars($comment['comment'])) ?>
+        </p>
+
+        <a href="index.php?action=reportComment&amp;id=<?= $comment['id'] ?>&amp;postId=<?= $post['id'] ?>">Signaler</a>
+    </div>
+    <?php
+    }
+    ?>
+</div>
+<?php $content = ob_get_clean(); ?>
+
+<?php require('template.php'); ?>
+
 {
 ?>
 <div id="commentZone">
